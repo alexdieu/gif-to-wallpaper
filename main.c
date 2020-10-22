580285060dbea6bd0d7918194d1a04f91f9f6b9d
Alexdieu <alexdieuveult@gmail.com>
2020-10-22 21:19:22 +0200

    Update main.c

--- main.c
+++ main.c
@@ -8,20 +8,20 @@ using namespace std;
 int main(void)         
 {	
     while( 0 < 1 ){
-			std::string s = ("C:\\Users\\alexd\\Downloads\\GIF-Wallpaper-master\\GIF-Wallpaper-master\\tmp_files\\0.jpg");
+			std::string s = ("FIRSTPATHTOPNG");
 			SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (PVOID*)s.c_str(), SPIF_SENDCHANGE);
 			Sleep(50);
-			std::string z = ("C:\\Users\\alexd\\Downloads\\GIF-Wallpaper-master\\GIF-Wallpaper-master\\tmp_files\\1.jpg");
+			std::string z = ("SECONDPATHTOPNG");
 			SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (PVOID*)z.c_str(), SPIF_SENDCHANGE);
 			Sleep(50);
-			std::string e = ("C:\\Users\\alexd\\Downloads\\GIF-Wallpaper-master\\GIF-Wallpaper-master\\tmp_files\\2.jpg");
-			SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (PVOID*)e.c_str(), SPIF_SENDCHANGE);
-			Sleep(50);
-			std::string d = ("C:\\Users\\alexd\\Downloads\\GIF-Wallpaper-master\\GIF-Wallpaper-master\\tmp_files\\3.jpg");
+			std::string d = ("THIRDPATHTOPNG");
 			SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (PVOID*)d.c_str(), SPIF_SENDCHANGE);
 			Sleep(50);
-			std::string v = ("C:\\Users\\alexd\\Downloads\\GIF-Wallpaper-master\\GIF-Wallpaper-master\\tmp_files\\4.jpg");
+			std::string v = ("FOURTHPATHTOPNG");
 			SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (PVOID*)v.c_str(), SPIF_SENDCHANGE);
+			Sleep(50);
+	    		std::string e = ("FIFTHPATHTOPNG");
+			SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (PVOID*)e.c_str(), SPIF_SENDCHANGE);
 			Sleep(50);
 	}
 	
